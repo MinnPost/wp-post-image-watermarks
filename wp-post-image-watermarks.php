aa1f84dd86f1acebb589d77b7fccdc23c368f605
Jonathan Stegall <jstegall@minnpost.com>
2018-03-27 14:35:47 -0500

    this is pretty rough still, but it works to put the selected watermark on the image and save it. lots to fix, still

--- wp-post-image-watermarks.php
+++ wp-post-image-watermarks.php
@@ -55,8 +55,10 @@ class WP_Post_Image_Watermarks {
 		$this->version       = '0.0.1';
 		$this->slug          = 'wp-post-image-watermarks';
 
+		$this->thumbnail_image_field = '_mp_post_thumbnail_image';
+
 		$this->watermark_field      = '_mp_plus_icon_style';
-		$this->watermark_folder_url = get_theme_file_uri() . 'assets/img/icons/';
+		$this->watermark_folder_url = get_theme_file_uri() . '/assets/img/icons/';
 		$this->watermark_extension  = '.png';
 
 		$this->add_actions();
@@ -69,8 +71,62 @@ class WP_Post_Image_Watermarks {
 	private function add_actions() {
 		// add the watermark image editor
 		add_filter( 'wp_image_editors', array( $this, 'add_watermark_editor' ), 10, 1 );
+
+		// save watermark on post save
+		add_action( 'save_post', array( $this, 'save_watermark_image' ) );
+
 		// when an image is uploaded, add watermark if applicable
-		add_filter( 'wp_handle_upload', array( $this, 'handle_upload_callback' ), 10, 3 );
+		//add_filter( 'wp_handle_upload', array( $this, 'handle_upload_callback' ), 10, 3 );
+	}
+
+	/**
+	* Save watermark when post is saved
+	*
+	* @param int $post_id
+	*/
+	public function save_watermark_image( $post_id ) {
+			// Make sure we should be doing this action
+		if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE || ( isset( $_POST['post_type'] ) && 'page' === $_POST['post_type'] && ! current_user_can( 'edit_page', $post_id ) ) || ! current_user_can( 'edit_post', $post_id ) ) {
+			return;
+		}
+		// Filter the default overlay settings
+		//add_filter( 'watermark_image_filter_defaults', 'update_watermark_image_defaults', 10, 2 );
+		//Watermark_Image_Generator::generate_watermark( $text, $img_url, $post_id );
+
+		// hook to set whether or not this attachment needs to be watermarked
+		$watermarked = apply_filters( 'image_watermark_allowed', false, $post_id );
+
+		if ( false === $watermarked ) {
+			return;
+		}
+
+		$post_meta = get_post_meta( $post_id );
+
+		// see if there is a value on the post for the watermark field
+		if ( isset( $post_meta[ $this->watermark_field ][0] ) ) {
+			$watermark_file_name = $post_meta[ $this->watermark_field ][0];
+		} else {
+			return;
+		}
+
+		if ( isset( $post_meta[ $this->thumbnail_image_field ][0] ) ) {
+			$thumbnail_url = str_replace( get_site_url(), $_SERVER['DOCUMENT_ROOT'], $post_meta[ $this->thumbnail_image_field ][0] );
+		} else {
+			return;
+		}
+
+		$watermark_url = str_replace( get_site_url(), $_SERVER['DOCUMENT_ROOT'], $this->watermark_folder_url . $watermark_file_name . $this->watermark_extension );
+
+		$image = wp_get_image_editor( $thumbnail_url );
+		if ( ! is_wp_error( $image ) && is_callable( [ $image, 'stamp_watermark' ] ) ) {
+			$stamp   = imagecreatefrompng( $watermark_url );
+			$success = $image->stamp_watermark( $watermark_url );
+			if ( ! is_wp_error( $success ) ) {
+				error_log( 'save the image at ' . $thumbnail_url );
+				$image->save( $thumbnail_url );
+			}
+		}
+
 	}
 
 	/**
@@ -101,7 +157,7 @@ class WP_Post_Image_Watermarks {
 		//$attachment = $this->get_attachment( $upload['url'] );
 		$attachment_id = $this->get_attachment_id( $upload['url'] );
 		// if this attachment is not attached to a post, we can't do anything with it
-		if ( 0 == $attachment->post_parent ) {
+		if ( 0 == $post_id ) {
 			error_log( 'not an attachment' );
 			return $upload;
 		}
@@ -114,7 +170,7 @@ class WP_Post_Image_Watermarks {
 			return $upload;
 		}
 
-		$post_meta = get_post_meta( $attachment->post_parent );
+		$post_meta = get_post_meta( $post_id );
 
 		// see if there is a value on the post for the watermark field
 		if ( ! isset( $post_meta[ $this->watermark_field ] ) ) {
